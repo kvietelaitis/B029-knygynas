39b1078687c4439ae81ca560b3ed389aba3fc5f1
martynas <martynaskvietelaitis@icloud.com>
2026-04-29 20:16:23 +0300

    models

--- Knygynas/Data/ApplicationDbContext.cs
+++ Knygynas/Data/ApplicationDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Knygynas.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Knygynas.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
 {
+    public DbSet<Book> Books { get; set; }
 }
---
+++ Knygynas/Models/BookCategory.cs
@@ -0,0 +1,9 @@
+namespace Knygynas.Models;
+
+public class BookCategory
+{
+    public int BookID { get; set; }
+    public Book Book { get; set; }
+    public int CategoryID { get; set; }
+    public Category Category { get; set; }
+}
